215e05e33dec6b995e76617b75999834e5b2fa21
PC-W136\Kazuki_Ishii <kishii@system-renovate.co.jp>
2023-01-06 10:26:26 +0900

    ファイル配置変更

---
+++ アプリ設定情報取得/src/メイン処理/設計情報書き出し/アプリへの出力/app/action-config.js
@@ -0,0 +1,6 @@
+/**
+ * アクション設定の書き出し
+ * @param {number | string} targetAppId 書き出し対象のアプリID
+ * @param {number | string} configAppId 書き出し先のアプリID
+ */
+function designInfoOutput(targetAppId, configAppId) {}
